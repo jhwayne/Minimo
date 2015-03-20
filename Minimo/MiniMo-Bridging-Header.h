8778ebf6d11a2ffd519b622b101b6b4d1d11c2b8
Jake Wayne <jhwayne@umich.edu>
2015-03-20 18:00:56 -0400

    parse integration

---
+++ Minimo/MiniMo-Bridging-Header.h
@@ -0,0 +1,15 @@
+//
+//  Header.h
+//  Minimo
+//
+//  Created by Jake Wayne on 3/20/15.
+//  Copyright (c) 2015 Team Mo. All rights reserved.
+//
+
+//
+//  Use this file to import your target's public headers that you would like to expose to Swift.
+//
+
+#import <Parse/Parse.h>
+#import <ParseUI/ParseUI.h>
+#import <Bolts/BFTask.h>
--- Minimo/Minimo/AppDelegate.swift
+++ Minimo/Minimo/AppDelegate.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import Parse
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -16,6 +17,12 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
         // Override point for customization after application launch.
+        
+        Parse.setApplicationId("tZekq7ApZ9hP2jWlMq5BvKRBLLsKvDWWh5Kyqr9R", clientKey:"m8VmKfKK9pZQMdOqmvw9CtQknxodCmlvXmQ8Ye27")
+        var object = PFObject(className: "User")
+        object.addObject("Jake", forKey: "Name")
+        object.save()
+        
         return true
     }
 
