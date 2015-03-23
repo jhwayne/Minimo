ef7728ecb595c576c90ed448e36d7b2d7889bc0a
Jake Wayne <jhwayne@umich.edu>
2015-03-22 22:29:52 -0400

    parse signinup

---
+++ Minimo/Minimo/SignUpInViewController.swift
@@ -0,0 +1,128 @@
+//
+//  SignUpInViewController.swift
+//  Minimo
+//
+//  Created by Jake Wayne on 3/22/15.
+//  Copyright (c) 2015 Team Mo. All rights reserved.
+//
+
+import UIKit
+import Parse
+
+class SignUpInViewController: UIViewController {
+    
+    @IBOutlet weak var activityIndicator: UIActivityIndicatorView!
+    @IBOutlet weak var message: UILabel!
+    @IBOutlet weak var emailAddress: UITextField!
+    @IBOutlet weak var password: UITextField!
+    
+    func processSignUp() {
+        
+        var userEmailAddress = emailAddress.text
+        var userPassword = password.text
+        
+        // Ensure username is lowercase
+        userEmailAddress = userEmailAddress.lowercaseString
+        
+        // Start activity indicator
+        activityIndicator.hidden = false
+        activityIndicator.startAnimating()
+        
+        // Create the user
+        var user = PFUser()
+        user.username = userEmailAddress
+        user.password = userPassword
+        user.email = userEmailAddress
+        
+        user.signUpInBackgroundWithBlock {
+            (succeeded: Bool!, error: NSError!) -> Void in
+            if error == nil {
+                dispatch_async(dispatch_get_main_queue()) {
+                    self.performSegueWithIdentifier("signInToNavigation", sender: self)
+                }
+            } else {
+                self.activityIndicator.stopAnimating()
+                
+                if let message: AnyObject = error!.userInfo!["error"] {
+                    self.message.text = "\(message)"
+                }
+            }
+        }
+    }
+    @IBAction func signUp(sender: AnyObject) {
+        
+        // Build the terms and conditions alert
+        let alertController = UIAlertController(title: "Agree to terms and conditions",
+            message: "Click I AGREE to signal that you agree to the End User Licence Agreement.",
+            preferredStyle: UIAlertControllerStyle.Alert
+        )
+        alertController.addAction(UIAlertAction(title: "I AGREE",
+            style: UIAlertActionStyle.Default,
+            handler: { alertController in self.processSignUp()})
+        )
+        alertController.addAction(UIAlertAction(title: "I do NOT agree",
+            style: UIAlertActionStyle.Default,
+            handler: nil)
+        )
+        
+        // Display alert
+        self.presentViewController(alertController, animated: true, completion: nil)
+    }
+    
+    @IBAction func signIn(sender: AnyObject) {
+        activityIndicator.hidden = false
+        activityIndicator.startAnimating()
+        
+        var userEmailAddress = emailAddress.text
+        userEmailAddress = userEmailAddress.lowercaseString
+        
+        var userPassword = password.text
+        
+        PFUser.logInWithUsernameInBackground(userEmailAddress, password:userPassword) {
+            (user: PFUser!, error: NSError!) -> Void in
+            if user != nil {
+                dispatch_async(dispatch_get_main_queue()) {
+                    self.performSegueWithIdentifier("signInToNavigation", sender: self)
+                }
+            } else {
+                self.activityIndicator.stopAnimating()
+                
+                if let message: AnyObject = error!.userInfo!["error"] {
+                    self.message.text = "\(message)"
+                }
+            }
+        }
+    }
+    
+    @IBAction func signOut(sender: AnyObject) {
+        
+        PFUser.logOut()
+        
+        let storyboard = UIStoryboard(name: "Main", bundle: nil)
+        let vc = storyboard.instantiateViewControllerWithIdentifier("SignInViewController") as UIViewController
+        self.presentViewController(vc, animated: true, completion: nil)
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        activityIndicator.hidden = true
+        activityIndicator.hidesWhenStopped = true
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+}
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
--- Minimo/Minimo/ViewController.swift
+++ Minimo/Minimo/ViewController.swift
@@ -2,7 +2,7 @@
 //  ViewController.swift
 //  Minimo
 //
-//  Created by Justin Platz on 3/20/15.
+//  Created by Justin Platz and Jake Wayne on 3/20/15.
 //  Copyright (c) 2015 Team Mo. All rights reserved.
 //
 
@@ -11,56 +11,18 @@ import Parse
 
 class ViewController: UIViewController, UITextFieldDelegate {
     
-    @IBOutlet var usernameTextField: UITextField!
-    @IBOutlet var passwordTextField: UITextField!
-    @IBOutlet var emailTextField: UITextField!
-    @IBOutlet var messageLabel: UILabel!
-    
-    @IBAction func loginVerifyButton(sender: AnyObject) {
+    @IBAction func signOut(sender: AnyObject) {
         
-        var usrEntered = usernameTextField.text
-        var pwdEntered = passwordTextField.text
-        var emlEntered = emailTextField.text
+        PFUser.logOut()
         
-        if usrEntered != "" && pwdEntered != "" && emlEntered != "" {
-            userSignUp()
-        }
-        else {
-            self.messageLabel.text = "All Fields Required"
-        }
-        
-    }
-    
-    func userSignUp() {
-        var usrEntered = usernameTextField.text
-        var pwdEntered = passwordTextField.text
-        var emlEntered = emailTextField.text
-        
-        var user = PFUser()
-        
-        user.username = usrEntered
-        user.password = pwdEntered
-        user.email = emlEntered
-        
-        user.signUpInBackgroundWithBlock {
-            (succeeded: Bool!, error: NSError!) -> Void in
-            if error == nil {
-                // Hooray! Let them use the app now.
-                self.messageLabel.text = "User Signed Up";
-            } else {
-                // Show the errorString somewhere and let the user try again.
-            }
-        }
-        
+        let storyboard = UIStoryboard(name: "Main", bundle: nil)
+        let vc = storyboard.instantiateViewControllerWithIdentifier("SignUpInViewController") as UIViewController
+        self.presentViewController(vc, animated: true, completion: nil)
     }
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
-        
-        self.usernameTextField.delegate = self
-        self.passwordTextField.delegate = self
-        self.emailTextField.delegate = self
+
         
     }
     
