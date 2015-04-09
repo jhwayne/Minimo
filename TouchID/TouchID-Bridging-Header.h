82a26087f8e5637ab3b3fc71bcf9b682a54af149
Jake Wayne <jhwayne@umich.edu>
2015-04-08 22:44:50 -0400

    frameworks

---
+++ TouchID/TouchID-Bridging-Header.h
@@ -0,0 +1,15 @@
+//
+//  TouchID-Bridging-Header.h
+//  TouchID
+//
+//  Created by Jake Wayne on 4/8/15.
+//  Copyright (c) 2015 AppCoda. All rights reserved.
+//
+
+#ifndef TouchID_TouchID_Bridging_Header_h
+#define TouchID_TouchID_Bridging_Header_h
+
+
+#endif
+
+#import <Parse/Parse.h>
--- TouchID/TouchID/AppDelegate.swift
+++ TouchID/TouchID/AppDelegate.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import Parse
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -16,6 +17,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
         // Override point for customization after application launch.
+        Parse.setApplicationId("tZekq7ApZ9hP2jWlMq5BvKRBLLsKvDWWh5Kyqr9R", clientKey:"m8VmKfKK9pZQMdOqmvw9CtQknxodCmlvXmQ8Ye27")
         return true
     }
 
